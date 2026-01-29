2b291369ab67055d470f745f50a342646e1bfefb
xkoderfx13 <koderfx@gmail.com>
2026-01-29 17:53:19 +0300

    Add Wasmer plan file

--- myproject/settings.py
+++ myproject/settings.py
@@ -82,15 +82,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-if os.environ.get('WASMER_ENV'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DB_NAME'),
